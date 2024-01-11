61ca7539720d70cceeec54216ce8b17a14df0d62
Vishal Ukade <vishalukade5@gmail.com>
2024-01-11 13:23:00 +0530

    Fix firebase initialization

---
+++ src/firebase.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+});
+
+export const auth = app.auth();
+const firestore = app.firestore();
+export const db = {
+  folders: firestore.collection("folders"),
+  files: firestore.collection("files"),
+  formatDoc: (doc) => {
+    return { id: doc.id, ...doc.data() };
+  },
+  getCurrentTimestamp: firebase.firestore.FieldValue.serverTimestamp,
+};
+export const storage = app.storage();
+
+export default app;
+// Commit on 2024-01-11T13:23:00
