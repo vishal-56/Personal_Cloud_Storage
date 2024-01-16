a9c56b6f56522b0d65eb6b9bb8d5e6d5ef0c69a9
Vishal Ukade <vishalukade5@gmail.com>
2024-01-16 10:45:00 +0530

    Improve AlertContext

---
+++ src/contexts/AlertContext.jsx
@@ -0,0 +1,47 @@
+import { useContext, useState, useEffect, createContext } from "react";
+import PropTypes from "prop-types";
+import { Alert } from "../components/Alert";
+
+AlertProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+const AlertContext = createContext();
+
+export function useAlert() {
+  return useContext(AlertContext);
+}
+
+export function AlertProvider({ children }) {
+  const [alert, setAlert] = useState(null);
+  const [timeoutId, setTimeoutId] = useState(null);
+
+  useEffect(() => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    if (alert) {
+      setTimeoutId(
+        setTimeout(() => {
+          setAlert(null);
+        }, 2000),
+      );
+    }
+  }, [alert]);
+
+  function showAlert(className, message) {
+    setAlert({ className: className, message: message });
+  }
+
+  const value = {
+    showAlert,
+  };
+
+  return (
+    <AlertContext.Provider value={value}>
+      {alert && <Alert alert={alert} />}
+      {children}
+    </AlertContext.Provider>
+  );
+}
+// Commit on 2024-01-16T10:45:00
