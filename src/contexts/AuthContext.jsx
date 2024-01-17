5d925f20d1b134e78be08e5f3acaf4e61572656f
Vishal Ukade <vishalukade5@gmail.com>
2024-01-17 11:42:00 +0530

    Fix AuthContext logic

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,68 @@
+import { useContext, useState, useEffect, createContext } from "react";
+import { auth } from "../firebase";
+import PropTypes from "prop-types";
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    return auth.signOut();
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  function updateEmail(email) {
+    return currentUser.verifyBeforeUpdateEmail(email);
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
+// Commit on 2024-01-17T11:42:00
