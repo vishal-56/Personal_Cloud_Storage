72243eb652d9cbd4382d45f7d676f243ff719d00
Vishal Ukade <vishalukade5@gmail.com>
2024-01-19 10:53:00 +0530

    Fix Alert component UI

---
+++ src/components/Alert.jsx
@@ -0,0 +1,19 @@
+import { CheckCircle2, XCircle } from "lucide-react";
+
+export function Alert({ alert }) {
+  return (
+    <div className="toast toast-center toast-middle z-50 w-screen max-w-lg">
+      <div className={"alert flex text-white " + alert.className} role="alert">
+        {alert.className === "alert-success" ? (
+          <CheckCircle2 className="shrink-0" />
+        ) : (
+          <XCircle className="shrink-0" />
+        )}
+        <span className="flex-grow text-wrap text-center ">
+          {alert.message}
+        </span>
+      </div>
+    </div>
+  );
+}
+// Commit on 2024-01-19T10:53:00
