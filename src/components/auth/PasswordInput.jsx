bcda1f25c479ae6d25299fd36d8dad1810e3a421
Vishal Ukade <vishalukade5@gmail.com>
2024-01-31 12:07:00 +0530

    Improve PasswordInput

---
+++ src/components/auth/PasswordInput.jsx
@@ -0,0 +1,29 @@
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { useState } from "react";
+
+export function PasswordInput(params) {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <div className="relative">
+      <input
+        {...params.inputParams}
+        className={
+          "input input-bordered w-full pr-9" +
+          (params.hasError ? " input-error" : "")
+        }
+        type={showPassword ? "text" : "password"}
+      />
+      <div
+        className="absolute bottom-0 right-0 top-0 flex cursor-pointer items-center pr-3"
+        onClick={() => setShowPassword((show) => !show)}
+      >
+        {showPassword ? (
+          <EyeIcon size={15} className="stroke-slate-400" />
+        ) : (
+          <EyeOffIcon size={15} className="stroke-slate-400" />
+        )}
+      </div>
+    </div>
+  );
+}
+// Commit on 2024-01-31T12:07:00
