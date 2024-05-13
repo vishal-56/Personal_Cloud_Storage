982ca7736f0a0dc3e4fa0db8de4d07ee972d043d
Vishal Ukade <vishalukade5@gmail.com>
2024-05-13 13:09:00 +0530

    Fix FileItem rendering

--- src/components/FileItem.jsx
+++ src/components/FileItem.jsx
@@ -31,3 +31,4 @@ export default function FileItem({ file, openConformationModal }) {
 // Commit on 2024-01-23T12:42:00
 // Commit on 2024-02-29T11:25:00
 // Commit on 2024-04-06T10:04:00
+// Commit on 2024-05-13T13:09:00
