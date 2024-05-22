44129472e9393793fe747c65bff8535ceb1666ad
Vishal Ukade <vishalukade5@gmail.com>
2024-05-22 12:37:00 +0530

    Fix PrivateRoute redirect

--- src/components/auth/PrivateRoute.jsx
+++ src/components/auth/PrivateRoute.jsx
@@ -20,3 +20,4 @@ export default function PrivateRoute({ component: Component, ...rest }) {
 // Commit on 2024-02-01T10:17:00
 // Commit on 2024-03-09T17:36:00
 // Commit on 2024-04-15T17:36:00
+// Commit on 2024-05-22T12:37:00
