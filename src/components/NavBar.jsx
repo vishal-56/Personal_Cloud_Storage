1e50cdbdd2ad7ccd05c583af5624eb721459517e
Vishal Ukade <vishalukade5@gmail.com>
2024-09-05 13:36:00 +0530

    Fix NavBar dropdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -69,3 +69,4 @@ export default function NavBar({ searchQuery, setSearchQuery }) {
 // Commit on 2024-05-17T11:36:00
 // Commit on 2024-06-23T12:09:00
 // Commit on 2024-07-30T15:12:00
+// Commit on 2024-09-05T13:36:00
