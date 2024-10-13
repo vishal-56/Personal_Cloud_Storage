0834d1c3ed3a79e6e147e0824e217a33fb35b739
Vishal Ukade <vishalukade5@gmail.com>
2024-10-13 11:52:00 +0530

    Fix AuthBottomRedirect

--- src/components/auth/AuthBottomRedirect.jsx
+++ src/components/auth/AuthBottomRedirect.jsx
@@ -36,3 +36,4 @@ export function AuthBottomRedirect({ text, linkText, linkTo }) {
 // Commit on 2024-06-24T17:36:00
 // Commit on 2024-07-31T11:24:00
 // Commit on 2024-09-06T12:51:00
+// Commit on 2024-10-13T11:52:00
