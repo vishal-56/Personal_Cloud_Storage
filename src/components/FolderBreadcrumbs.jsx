245dffa822ed1843c67561d771ff00391a4a566d
Vishal Ukade <vishalukade5@gmail.com>
2024-11-15 14:15:00 +0530

    Fix FolderBreadcrumbs

--- src/components/FolderBreadcrumbs.jsx
+++ src/components/FolderBreadcrumbs.jsx
@@ -44,3 +44,4 @@ export default function Breadcrumbs({ currentFolder }) {
 // Commit on 2024-07-27T12:12:00
 // Commit on 2024-09-02T16:29:00
 // Commit on 2024-10-09T10:43:00
+// Commit on 2024-11-15T14:15:00
