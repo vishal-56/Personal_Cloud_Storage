6679cabcbf691ca854d5613a3948d3640778b671
Vishal Ukade <vishalukade5@gmail.com>
2024-11-16 17:16:00 +0530

    Fix FolderItem selection

--- src/components/FolderItem.jsx
+++ src/components/FolderItem.jsx
@@ -31,3 +31,4 @@ export default function FolderItem({ folder, onDelete }) {
 // Commit on 2024-07-28T14:49:00
 // Commit on 2024-09-03T13:38:00
 // Commit on 2024-10-10T17:54:00
+// Commit on 2024-11-16T17:16:00
